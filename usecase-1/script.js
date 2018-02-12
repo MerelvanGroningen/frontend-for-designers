2ab52ef1dd28c5249189160e5475beb2adca62c8
MerelvanGroningen <Merel.van.groningen@hva.nl>
2018-02-12 23:05:59 +0100

    fixed clicked on hearts on images

--- usecase-1/script.js
+++ usecase-1/script.js
@@ -1,8 +1,18 @@
 function changeImage() {
-    var image = document.getElementById('heart');
-    if (image.src.match("heartred")) {
-        image.src = "img/heartwhite.png";
-    } else {
-        image.src = "img/heartred.png";
-    }
+    // var image = document.getElementById('heart');
+    // if (image.src.match("heartred")) {
+    //     image.src = "img/heartwhite.png";
+    // } else {
+    //     image.src = "img/heartred.png";
+    // }
 }
+
+$(document).ready(function() {
+  $(".heart").on("click", function() {
+      if ($(this).attr("src") == "img/heartred.png") {
+          $(this).attr("src", "img/heartwhite.png");
+      } else {
+          $(this).attr("src", "img/heartred.png");
+      }
+  });
+});
