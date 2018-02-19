0d77af28d9dcf1253e65f53053209e595f0bbef8
MerelvanGroningen <Merel.van.groningen@hva.nl>
2018-02-19 22:41:14 +0100

    Jquery naar Javascript en watchlist counter

--- usecase-1/script.js
+++ usecase-1/script.js
@@ -1,18 +1,63 @@
-function changeImage() {
-    // var image = document.getElementById('heart');
-    // if (image.src.match("heartred")) {
-    //     image.src = "img/heartwhite.png";
-    // } else {
-    //     image.src = "img/heartred.png";
-    // }
-}
+// Deze code werkt alleen voor het eerste element .heart
+//
+// function clickImage() {
+//   var image = document.querySelector('.heart');
+//   if (image.src.match("heartred")) {
+//       image.src = "img/heartwhite.png";
+//   } else {
+//       image.src = "img/heartred.png";
+//   }
+// }
+//
+// var image = document.querySelector('.heart');
+// image.addEventListener('click', clickImage, false);
 
-$(document).ready(function() {
-  $(".heart").on("click", function() {
-      if ($(this).attr("src") == "img/heartred.png") {
-          $(this).attr("src", "img/heartwhite.png");
-      } else {
-          $(this).attr("src", "img/heartred.png");
-      }
+var watchlist_total = 0;
+// forEach werkt wel, hij pakt hier in de fuctie alle elementen uit de array (je loopt door elke image heen en voegt overal apart die addEventListener toe)
+var images = document.querySelectorAll('.heart');
+images.forEach (function(image) {
+  image.addEventListener('click', function(){
+    if (image.src.match("heartred")) {
+        image.src = "img/heartwhite.png";
+        watchlist_total--;
+    } else {
+        image.src = "img/heartred.png";
+        watchlist_total++;
+    }
+    document.getElementById('watchlist').innerHTML = "watchlist(" + watchlist_total + ")";
   });
 });
+
+
+
+// loop geprobeerd, click werkte wel overal. Alleen de laatste werd aangepast op een of andere manier..
+//
+// var images = document.querySelectorAll('.heart')
+// for (var x = 0; x < images.length; x++) {
+//   var image = images[x]
+//   image.addEventListener('click', function(){
+//     alert("click: " + image);
+//   if (image.getAttribute("src").match("heartred")) {
+//       image.src = "img/heartwhite.png";
+//   } else {
+//       image.src = "img/heartred.png";
+//   }
+//
+//   });
+// }
+
+
+
+// eerste keer proberen querySelectorAll, li.heart klopte niet, want het is img.heart
+//
+// function changeImage() {        //declareren van functie
+//   var hearts = document.querySelectorAll("li.heart"); // haal alle hearts
+//   if (hearts.src.match("heartred")) {               // if afbeelding is rood
+//     hearts.src = "img/heartwhite.png";              // maak wit
+//   } else {                                          // anders
+//     hearts.src = "img/heartred.png";                // maak wit
+//   }
+// }
+//
+//
+//image.addEventListener('click', changeImage, false);
