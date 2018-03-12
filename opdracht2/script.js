bdbd74a014762057172fe1f3bcf7b0ecad604e4b
MerelvanGroningen <Merel.van.groningen@hva.nl>
2018-03-12 16:13:25 +0100

    Door scroll heen klikken en tikken
    Javascript nog niet compleet

--- opdracht2/script.js
+++ opdracht2/script.js
@@ -1,1 +1,11 @@
-var slides = document.queryselectorAll(sliderSelector);
+//var slides = document.queryselectorAll(sliderSelector);
+//var btnright = document.queryselector(.arrowRight);
+//var btnleft = document.queryselector(.arrowLeft);
+
+
+var scrollcontainer = document.querySelector('main section');
+var btnright = document.querySelector(".arrowRight");
+var btnleft = document.querySelector(".arrowLeft");
+
+console.log("scrollcontainer.offsetLeft: " + scrollcontainer.offsetLeft);
+console.log("scrollcontainer.offsetWidth: " + scrollcontainer.offsetWidth);
